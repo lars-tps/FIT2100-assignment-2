e5ce7676f1c06e472336103827af57db3f77cdd5
HardDisco <49183778+lars-tps@users.noreply.github.com>
2022-10-08 14:57:51 +0800

    init read_processes function files

---
+++ src/read_processes.c
@@ -0,0 +1,24 @@
+/*
+* Reads process information from a file descriptor and place them in a queue.
+* 
+* DESCRIPTION :
+*     placeholder
+* 
+* PUBLIC FUNCTIONS :
+*
+* REFERENCES :
+*       file reading code referenced from  
+*  https://www.youtube.com/watch?v=X-1qodkHCHo
+* 
+* AUTHOR:         Tan Pei Sheng
+* STUDENT ID:     32185235
+* START DATE:     8 Oct 2022
+* LAST MODIFIED:  8 Oct 2022
+* 
+*/
+
+#include "./read_processes.h"
+
+node** read_from_file(int fd) {
+
+}
---
+++ src/read_processes.h
@@ -0,0 +1,5 @@
+#include "./libraries.h"
+#include "./process_queue.h"
+#include "./pcb.h"
+
+node** read_from_file(int fd);
